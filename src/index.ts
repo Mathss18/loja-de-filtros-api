21cd8390b39ca3f62bdf465bcabc4f454dc72c4f
Matheus Filho <45442720+Mathss18@users.noreply.github.com>
2023-10-12 00:17:15 -0300

    feat: remove "/" route

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use("/", (req, res) => {
-  res.json({ message: "it works!" });
-});
 app.use("/orders", orderRoutes);
 
 app.use((req, res, next) => {
