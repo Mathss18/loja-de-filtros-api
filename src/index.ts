1a50d525cf6650bfd5a123521f2602f31223706e
Matheus <esdc1984@gmail.com>
2023-10-11 23:21:55 -0300

    chore: add base route

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use("/", (req, res) => {
+  res.json({ message: "it works!" }).send();
+});
 app.use("/orders", orderRoutes);
 
 app.use((req, res, next) => {
