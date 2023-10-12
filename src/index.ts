a9558f8911c59a2e14016083bf898da72887832f
Matheus Filho <45442720+Mathss18@users.noreply.github.com>
2023-10-11 23:53:26 -0300

    fix: remove send()

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/", (req, res) => {
-  res.json({ message: "it works!" }).send();
+  res.json({ message: "it works!" });
 });
 app.use("/orders", orderRoutes);
 
